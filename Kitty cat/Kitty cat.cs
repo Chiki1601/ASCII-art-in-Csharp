9e3bcb57e64e83e6b1cc21a4b8106740f134e871
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-02 10:26:56 +0530

    Create Kitty cat.cs

---
+++ Kitty cat/Kitty cat.cs
@@ -0,0 +1,31 @@
+
+using System;
+class HelloWorld {
+  static void Main() {
+    //Kitty teddy
+
+
+Console.WriteLine("´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´\n");
+Console.WriteLine(" ´´´´´´¶¶´´´´¶¶¶¶¶´´¶¶¶¶´¶¶¶¶´´\n");
+Console.WriteLine(" ´´´´´´¶´´´´´´´´´´¶¶¶¶´¶¶´´´´¶´\n");
+Console.WriteLine(" ´´´´´´¶´´´´´´´´´´¶´¶¶¶¶¶¶´´´¶´\n");
+Console.WriteLine(" ´´´´´¶´´´´´´´´´´¶¶¶¶¶´´´¶¶¶¶¶´\n");
+Console.WriteLine("´´´´¶´´´´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´\n");
+Console.WriteLine(" ´´´¶´´´´´´´´´´´´´´´´´´´¶¶¶¶¶´´\n");
+Console.WriteLine(" ´¶¶¶´´´´´¶´´´´´´´´´´´´´´´´´¶´´\n");
+Console.WriteLine("´´´¶´´´´¶¶´´´´´´´´´´´´´´´´´¶´´\n");
+Console.WriteLine(" ´´´¶¶´´´´´´´´´´´´´´´´¶¶´´´´¶´´\n");
+Console.WriteLine(" ´´¶¶¶´´´´´´´´´¶¶¶´´´´¶¶´´´¶¶´´\n");
+Console.WriteLine(" ´´´´´¶¶´´´´´´´´´´´´´´´´´´¶¶¶´´\n");
+Console.WriteLine(" ´´´´´´´¶¶¶´´´´´´´´´´´´´¶¶¶´´´´\n");
+Console.WriteLine(" ´´´¶¶¶¶¶´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´\n");
+Console.WriteLine(" ´´´¶´´´´¶¶¶¶¶´´´´¶¶¶¶´´´¶´´´´´\n");
+Console.WriteLine(" ´´´¶´´´´¶¶¶´¶¶¶¶¶¶¶¶´´´¶¶¶´´´´\n");
+Console.WriteLine("´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´¶¶¶¶¶´´´¶¶´´\n");
+Console.WriteLine(" ´´¶´´´´´´¶¶¶¶¶¶¶¶¶¶¶´´´´´´´¶´´\n");
+Console.WriteLine("´¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´¶´´\n");
+Console.WriteLine("´´¶´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´¶´´\n");
+Console.WriteLine("´´¶¶´´´´´´´¶¶´´´´¶¶´´´´´´¶¶´´´\n");
+Console.WriteLine("´´´´¶¶¶¶¶¶¶´´´´´´´´¶¶¶¶¶¶´´´´´\n");
+  }
+}
