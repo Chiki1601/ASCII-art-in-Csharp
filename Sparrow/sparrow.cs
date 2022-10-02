f36b2beb30931c3004031ae48114c61895e4d91a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-02 13:00:22 +0530

    Create sparrow.cs

---
+++ Sparrow/sparrow.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+Console.WriteLine("______________¶¶¶¶¶¶¶¶\n");
+Console.WriteLine("_¶¶¶__________¶¶¶¶_¶¶\n");
+Console.WriteLine("_¶¶_¶¶¶_________¶¶¶___¶¶\n");
+Console.WriteLine("_¶__¶¶¶¶_______¶¶____¶¶\n");
+Console.WriteLine("_¶___¶¶¶¶_____¶¶____¶¶\n");
+Console.WriteLine("_¶¶___¶¶¶¶____¶¶___¶¶\n");
+Console.WriteLine("_¶¶____¶¶¶¶¶¶__¶____¶¶\n");
+Console.WriteLine("_¶¶______¶¶¶_¶¶____¶¶\n");
+Console.WriteLine("_¶_______¶¶_¶___¶¶¶¶¶¶¶\n");
+Console.WriteLine("_¶¶_______¶¶¶__¶¶_ö_¶¶¶\n");
+Console.WriteLine("_¶¶_______¶¶¶¶¶¶¶____¶¶¶¶\n");
+Console.WriteLine("__¶¶___________¶¶¶\n");
+Console.WriteLine("__¶¶¶___________¶¶\n");
+Console.WriteLine("___¶¶¶¶________¶¶\n");
+Console.WriteLine("_____¶¶_______¶¶\n");
+Console.WriteLine("_____¶¶¶¶¶¶______¶¶\n");
+Console.WriteLine("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_______¶¶\n");
+Console.WriteLine("_¶___________¶¶¶¶\n");
+Console.WriteLine("_¶¶________¶¶¶¶¶\n");
+Console.WriteLine("_¶¶______¶¶¶¶¶¶¶¶¶\n");
+Console.WriteLine("__¶¶¶¶_____¶¶\n");
+Console.WriteLine("___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\n");
+		}
+	}
+}
