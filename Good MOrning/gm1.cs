a3bd915e36c187f68c8ffe7d87ce7414ff2f74a2
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:09:23 +0530

    Create gm1.cs

---
+++ Good MOrning/gm1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+// good morning
+     
+Console.WriteLine("🌷🌹🍃🌹🌷G\n");
+Console.WriteLine("┏╮╭┓┏╮╭┓ O\n");
+Console.WriteLine("╰╭╮╯╰╭╮╯ O\n");
+Console.WriteLine("╭╰╯╮╭╰╯╮ D\n");
+Console.WriteLine("┗╯╰┛┗╯╰┛ \n");
+Console.WriteLine("🌷🌹🍃🌹🌷 M\n");
+Console.WriteLine("┏╮╭┓┏╮╭┓  O\n");
+Console.WriteLine("╰╭╮╯╰╭╮╯  R\n");
+Console.WriteLine("╭╰╯╮╭╰╯╮  N\n");
+Console.WriteLine("┗╯╰┛┗╯╰┛   I\n");
+Console.WriteLine("🌷🌹🍃🌹🌷 N\n");
+Console.WriteLine("┏╮╭┓┏╮╭┓  G\n");
+Console.WriteLine("╰╭╮╯╰╭╮╯  \n");
+Console.WriteLine("╭╰╯╮╭╰╯╮  D\n");
+Console.WriteLine("┗╯╰┛┗╯╰┛  E\n");
+Console.WriteLine("🌷🌹🍃🌹🌷 A\n");
+Console.WriteLine("      FRIEND     \n");
+		}
+	}
+}
